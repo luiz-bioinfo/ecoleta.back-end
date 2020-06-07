98a4f34459595a0e4e7729229c3d177b424b098a
Luiz Antonio <luizneto.ifnmg@gmail.com>
2020-06-07 20:33:32 -0300

    Third commit: validation with celebrate

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
@@ -20,8 +21,27 @@ routes.get('/', (req, res) => {
 
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
-routes.post('/points', upload.single('image'), pointsController.create);
 
 routes.get('/items', itemsController.index);
+
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      whatsapp: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      UF: Joi.string().required().length(2),
+      items: Joi.string().required()
+    })
+  }, {
+    abortEarly: false
+  }),
+  pointsController.create
+);
 
 export default routes;
